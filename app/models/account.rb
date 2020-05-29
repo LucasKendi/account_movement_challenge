9fbd46e87717c318be65b7088906cfb544274a53
LucasKendi <hiranolucas@gmail.com>
2020-05-29 13:50:07 -0300

    Add summary output and change transacoes.csv to test fraction values

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,22 @@
 class Account < ApplicationRecord
   validates :number, presence: true, uniqueness: true, numericality: true
   validates :balance, presence: true, numericality: true
+
+  include ActiveSupport::NumberHelper
+
+  def summary
+    "Conta #{number} com saldo de #{format_balance}"
+  end
+
+  private
+
+  def format_balance
+    number_to_currency(
+      balance.to_f / 100,
+      unit: 'R$',
+      separator: ',',
+      delimiter: '',
+      precision: 2
+    )
+  end
 end
--- lib/tasks/calculate_balance.rake
+++ lib/tasks/calculate_balance.rake
@@ -2,5 +2,13 @@ namespace :balance do
   task :calculate, %i[account_file transactions_file] => :environment do |_task, args|
     Rake::Task['accounts:read'].execute(args[:account_file])
     Rake::Task['transactions:read'].execute(args[:transactions_file])
+    Rake::Task['balance:summary'].execute
+  end
+
+  task summary: :environment do
+    accounts = Account.all
+    accounts.each do |acc|
+      puts acc.summary
+    end
   end
 end
